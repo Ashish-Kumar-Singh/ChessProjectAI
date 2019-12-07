84ac996a905abbd2b74625bb0f7cd27f5e79983e
Ashish Kumar singh <blr.ashu@gmail.com>
2019-12-07 20:20:56 +0000

    not workig but ez 40%

--- AIAgent.java
+++ AIAgent.java
@@ -96,8 +96,67 @@ public class AIAgent{
   }
   
 
-  public Move twoLevelsDeep(Stack possibilities){
-    Move selectedMove = new Move();
-    return selectedMove;
+  public Move twoLevelsDeep(Stack whitemoves, Stack blackmoves){
+    Stack whiteMovestack = whitemoves;
+    Stack blackMoves = blackmoves;
+    double initialMoveStrength = 0;
+    double finalMoveStrength= 0;
+    Square blackmove;//stores a random player move from the stack
+    Move move2play =null;//the final move to play
+
+    for(int i=0;i<whiteMovestack.size();i++){
+      Move selectedMove = (Move)whitemoves.pop();//Selects a random move from the stack
+      if ((selectedMove.getStart().getYC() < selectedMove.getLanding().getYC())
+            && (selectedMove.getLanding().getXC() == 3) && (selectedMove.getLanding().getYC() == 3)
+            || (selectedMove.getLanding().getXC() == 4) && (selectedMove.getLanding().getYC() == 3)
+            || (selectedMove.getLanding().getXC() == 3) && (selectedMove.getLanding().getYC() == 4)
+            || (selectedMove.getLanding().getXC() == 4) && (selectedMove.getLanding().getYC() == 4)) {
+      
+              initialMoveStrength = 0.5;
+          }
+      while(!blackMoves.empty()){//selected move loops through all the black moves and checks which coinciding landing sqare has most points
+        blackmove = (Square)blackMoves.pop();
+        if((selectedMove.getLanding().getXC() == blackmove.getXC())//if the landing of the selected move is the same as the start of opponent
+        && (selectedMove.getLanding().getYC()== blackmove.getYC())){//If the selected moves triestaking a piece
+          System.out.println(blackmove.getName());
+          System.out.println(selectedMove.getLanding().getName());    
+          if(blackmove.getName().equals("BlackQueen")){
+              initialMoveStrength = 10;
+            }
+            else if(blackmove.getName().equals("BlackRook")){
+              initialMoveStrength = 6;
+            }
+            else if(blackmove.getName().equals("BlackBishop"))
+            {
+              initialMoveStrength = 4;
+            }
+            else if (blackmove.getName().equals("BlackKnight")){
+              initialMoveStrength = 4;
+            }
+            else if(blackmove.getName().equals("BlackPawn")){
+              initialMoveStrength = 2;
+            }
+            else if(blackmove.getName().equals("BlackKing")){
+              initialMoveStrength = 1000;
+            }
+            
+        }
+        if(initialMoveStrength > finalMoveStrength){
+          finalMoveStrength = initialMoveStrength;
+          move2play = selectedMove;
+        }
+      }     
+    
+
+    if(finalMoveStrength > 0){
+      System.out.println(finalMoveStrength);
+      return move2play;
+    }else{
+      return selectedMove;
+    }
+  }
+      return move2play;
+    
+    
   }
 }
